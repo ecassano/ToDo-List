f1dcc0a0c07e1af246fecbb796aff6d614cefcf7
eduardosa54 <eduardo.sa@rio.rj.gov.br>
2024-10-15 09:12:24 -0300

    CSS e event handlers

--- src/components/TaskCreator.tsx
+++ src/components/TaskCreator.tsx
@@ -1,12 +1,29 @@
 import { PlusCircle } from 'phosphor-react';
+import { ChangeEvent, useState } from 'react';
 
-import styles from './task-creator.module.css';
+import styles from './taskCreator.module.css';
 
 const TaskCreator = () => {
+  const [task, setTask] = useState('');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value);
+  }
+
+  const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+  }
+
   return (
     <form className={styles['task-creator']}>
-      <input type="text" name="task" placeholder='Adicione uma nova tarefa' />
-      <button>Criar<PlusCircle size={16} weight={'bold'} /></button>
+      <input
+        type="text"
+        name="task"
+        placeholder='Adicione uma nova tarefa'
+        value={task}
+        onChange={handleInputChange}
+      />
+      <button type='submit'>Criar<PlusCircle size={16} weight={'bold'} /></button>
     </form>
   )
 }
